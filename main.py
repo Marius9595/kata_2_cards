580c5d3c2a557cc189dd8ba95079366f5a6bbb30
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-03-31 18:25:46 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+KATA CARDS
+
+CONTEXT:
+    There are two players and they play with cards with the following rank:
+         1 > 2 > 3 > 4 > 5 > 7 > 8 > 9 > J > Q > K
+
+BEHAVIOUR:
+
+    # one by one card is distribuited to each player, when the second one
+    has 2 cards, is ok the dealing
+
+    # The first card compites with other first card and the same logic for the
+    second one
+
+    # The bigger gives to player a point in each duel
+
+    # It can be happen a Tie if the same value card is dueled
+
+    # The score is result of sum of the two duels
+
+    # The player with the higgest score is the winner and the other losser
+"""
+
---
+++ tests.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_x(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
